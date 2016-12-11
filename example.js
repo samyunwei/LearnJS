cb1208a6b41b0203e8d43a5035cb9c3102c169cf
samyunwei <259375702@qq.com>
2016-12-12 00:57:19 +0800

    add new proj

---
+++ example.js
@@ -0,0 +1,53 @@
+/**
+ * Created by ddsss on 2016/12/12.
+ */
+
+
+/*
+var mod;
+var age;
+
+mod = "happy";
+age = 33;
+
+alert(mod);
+alert(age);
+
+ */
+
+// var hight = "about 5'10\" tall"
+// alert(hight)
+//
+// var beatles = Array()
+// beatles[0] = "john"
+// beatles[1] = "Paul"
+// beatles[2] = "George"
+// beatles[3] = "Ringo"
+
+// var beatles = ["john","Paul","Geogre","Ringo"]
+//
+// var lennon = ["john",1940,false]
+//
+// beatles[4] = lennon[1]
+
+// var lennon = Array()
+// lennon["name"] = "John"
+// lennon["year"] = 1940
+// lennon["living"] = false;
+
+
+// var lennon = Object()
+// lennon.name = "john"
+// lennon.year = 1940
+// lennon.living = false;
+
+
+var lennon = {name:"john",year:1940,living:false};
+
+// var beatles = Array()
+// beatles[0] = lennon
+
+var bealts = {};
+
+bealts.vocalist = lennon;
+
