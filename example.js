5b9b9ec4bb0870049e62029e78d1f79e0363f907
Sam <259375702@qq.com>
2016-12-16 00:59:12 +0800

    add

--- example.js
+++ example.js
@@ -62,8 +62,100 @@ alert(age);
 // alert("10" + 20);
 // alert(10 + 20);
 
-var year = 2010;
-var message1 = "The year is";
-message1 += year;
+// var year = 2010;
+// var message1 = "The year is";
+// message1 += year;
+//
+// alert(message1);
 
-alert(message1);
+// a = 1;
+// b = 2;
+//
+//
+// if(a > b)
+// {
+//     alert("The world has gone mad!");
+// }else{
+//     alert("all is well with the world");
+// }
+//
+// var my_mood = "happy";
+// var your_mood = "sad";
+//
+// if(my_mood ==your_mood)
+// {
+//     alert("We both fell the same");
+// }
+//
+// if(my_mood != your_mood)
+// {
+//     alert("we're felling different moods");
+// }
+//
+
+//
+// var a = false;
+// var b = "";
+//
+// if(a === b){
+//     alert("a equals b");
+// }
+//
+// if( !(1> 2))
+// {
+//     alert("All is well with the world!");
+// }
+
+//
+//
+// var count = 1;
+// do{
+//     alert(count);
+// }while(count < 1);
+
+// var beatles = Array("john","Paul","George","Ringo");
+// for(var count = 0;count < beatles.length;count++)
+// {
+//     alert(beatles[count]);
+// }
+
+// function shout() {
+//     var beatles = Array("John","Paul","George","Ringo");
+//     for(var count =0 ;count < beatles.length;count++)
+//     {
+//         alert(beatles[count]);
+//     }
+// }
+//
+// function multiply(num1,num2)
+// {
+//     var total = num1 * num2;
+//     alert(total);
+// }
+//
+// function convertToCelsius(temp) {
+//     var result = temp -32;
+//     result = result / 1.8;
+//     return result;
+// }
+//
+// function square(num) {
+//     var total =  num * num;
+//     return total;
+// }
+//
+// var total = 50;
+// var number = square(20);
+// alert(total);
+//shout();
+
+// var temp_fahrenheit = 95;
+// var temp_celsius = convertToCelsius(temp_fahrenheit);
+// alert(temp_fahrenheit);
+//
+// multiply(10,2);
+
+
+var num = 7.561;
+var num = Math.round(num);
+alert(num);
