39e6ec8b75dc59c7a1fcbf05e027686d52f31772
samyunwei <259375702@qq.com>
2017-01-08 17:07:32 +0800

    add gif

---
+++ Chapter10/script/prepareSlideshow.js
@@ -0,0 +1,22 @@
+/**
+ * Created by ddsss on 2017/1/8.
+ */
+function prepareSlideshow() {
+    if(!document.getElementsByTagName)
+    {
+        return false;
+    }
+    if(!document.getElementById)
+    {
+        return false;
+    }
+    if(!document.getElementById("linklist"))
+    {
+        return false;
+    }
+    if(!document.getElementById("preview"))
+    {
+        return false;
+    }
+
+}
