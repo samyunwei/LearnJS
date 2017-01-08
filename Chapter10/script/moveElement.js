55cb8dd5c52644a4309e61e8cf1833932c28d2cf
samyunwei <259375702@qq.com>
2017-01-08 18:17:14 +0800

    add movie

--- Chapter10/script/moveElement.js
+++ Chapter10/script/moveElement.js
@@ -11,6 +11,18 @@ function moveElement(elementID,final_x,final_y,interval) {
         return false;
     }
     var elem = document.getElementById(elementID);
+    if(elem.movement)
+    {
+        clearTimeout(elem.movement);
+    }
+    if(!elem.style.left)
+    {
+        elem.style.left = "0px";
+    }
+    if(!elem.style.top)
+    {
+        elem.style.top = "0px";
+    }
     var xpos = parseInt(elem.style.left);
     var ypos = parseInt(elem.style.top);
     if(xpos == final_x && ypos == final_y)
@@ -19,22 +31,26 @@ function moveElement(elementID,final_x,final_y,interval) {
     }
     if(xpos < final_x)
     {
-        xpos++;
+        dist = Math.ceil((final_x - xpos) / 10);
+        xpos = xpos + dist;
     }
     if(xpos > final_x)
     {
-        xpos--;
+        dist = Math.ceil((xpos - final_x) / 10);
+        xpos = xpos - dist;
     }
     if(ypos < final_y)
     {
-        ypos++;
+        dist = Math.ceil((final_y - ypos));
+        ypos = ypos + dist;
     }
     if(ypos > final_y)
     {
-        ypos--;
+        dist = Math.ceil((ypos - final_y) / 10);
+        ypos = ypos - dist;
     }
     elem.style.left = xpos + "px";
     elem.style.top = ypos + "px";
     var repeat = "moveElement('"+elementID+"',"+final_x+","+final_y+","+interval+")";
-    movement = setTimeout(repeat,interval);
+    elem.movement = setTimeout(repeat,interval);
 }
--- Chapter10/script/prepareSlideshow.js
+++ Chapter10/script/prepareSlideshow.js
@@ -14,9 +14,27 @@ function prepareSlideshow() {
     {
         return false;
     }
-    if(!document.getElementById("preview"))
-    {
-        return false;
-    }
+    var slideshow = document.createElement("div");
+    slideshow.setAttribute("id","slideshow");
+    var preview = document.createElement("img");
+    preview.setAttribute("src","images/topics.gif");
+    preview.setAttribute("alt","building blocks of web disign");
+    preview.setAttribute("id","preview");
+    slideshow.appendChild(preview);
+    var list = document.getElementById("linklist");
+    insertAfter(slideshow,list);
 
+    var links = list.getElementsByTagName("a");
+
+    links[0].onmouseover = function () {
+        moveElement("preview",-100,0,10);
+    }
+    links[1].onmouseover = function () {
+        moveElement("preview",-200,0,10);
+    }
+    links[2].onmouseover = function () {
+        moveElement("preview",-300,0,10);
+    }
 }
+
+addLoadEvent(prepareSlideshow);
