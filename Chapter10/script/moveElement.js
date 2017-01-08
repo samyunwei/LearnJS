472d05512b030b1359459b94214b07c38935aa33
samyunwei <259375702@qq.com>
2017-01-08 16:42:35 +0800

    add

---
+++ Chapter10/script/moveElement.js
@@ -0,0 +1,40 @@
+/**
+ * Created by ddsss on 2017/1/8.
+ */
+function moveElement(elementID,final_x,final_y,interval) {
+    if(!document.getElementById)
+    {
+        return false;
+    }
+    if(!document.getElementById(elementID))
+    {
+        return false;
+    }
+    var elem = document.getElementById(elementID);
+    var xpos = parseInt(elem.style.left);
+    var ypos = parseInt(elem.style.top);
+    if(xpos == final_x && ypos == final_y)
+    {
+        return true;
+    }
+    if(xpos < final_x)
+    {
+        xpos++;
+    }
+    if(xpos > final_x)
+    {
+        xpos--;
+    }
+    if(ypos < final_y)
+    {
+        ypos++;
+    }
+    if(ypos > final_y)
+    {
+        ypos--;
+    }
+    elem.style.left = xpos + "px";
+    elem.style.top = ypos + "px";
+    var repeat = "moveElement('"+elementID+"',"+final_x+","+final_y+","+interval+")";
+    movement = setTimeout(repeat,interval);
+}
